ba2ad4a8c212ceb8594531204cdaea1523e0a56e
Daniel Calota <calotad@coventry.ac.uk>
2019-10-29 11:39:24 +0000

    Added a function body for the snake

--- snakegame.py
+++ snakegame.py
@@ -7,5 +7,10 @@ def window():
 	window.bgcolor("#85ff40")
 	window.setup(width = 640, height = 480, startx = None, starty = None)
 	window.tracer(0)
-	window.mainloop()
 
+def snake_body():
+	body = turtle.Turtle()
+	body.shape("square")
+	body.penup()
+	body.color("black")
+	body.goto(0, 0)
