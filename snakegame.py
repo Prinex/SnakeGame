628061f8c0491cc4dda29676dab71472a10b945d
Daniel Calota <calotad@coventry.ac.uk>
2019-10-31 12:52:17 +0000

    Added function for movement

--- snakegame.py
+++ snakegame.py
@@ -1,8 +1,26 @@
 import turtle
-from tkinter import PhotoImage
+import time
 
+# movement functions
 
+delay = 0.1
 
+def move(head, direction):
+    if head.direction == "up":
+        y = head.ycor()
+        head.sety(y + 20)
+
+    if head.direction == "down":
+        y = head.ycor()
+        head.sety(y - 20)
+
+    if head.direction == "left":
+        x = head.xcor()
+        head.setx(x - 20)
+
+    if head.direction == "right":
+        x = head.xcor()
+        head.setx(x + 20)
 
 def game():
 
@@ -15,7 +33,7 @@ def game():
 
     
     # snake body 
-    turtleAvatar = "snakebody.gif"
+    turtleAvatar = "snakehead.gif"
     head = turtle.Turtle()
     head.speed(0)
     window.addshape(turtleAvatar)
@@ -23,10 +41,12 @@ def game():
     head.penup()
     head.color("#303324")
     head.goto(0, 0)
-    head.direction = "stop"
+    head.direction = "right"
 
     while True:
         window.update()
+        move(head, head.direction)
+        time.sleep(delay)
 
     window.mainloop()
 
