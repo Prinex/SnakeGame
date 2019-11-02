56c53546cc56f17fefc88bb6fc35d2b729f385c6
Prinex <38051195+Prinex@users.noreply.github.com>
2019-11-03 00:37:04 +0200

    Updated the program with: improvments for moving, script for score, script for collisioning with the borders and with the snake itself.

--- snakegame.py
+++ snakegame.py
@@ -5,11 +5,16 @@ import random
 
 delay = 0.1
 
+# score count
+score_count = 0
+
+highest_score = 0
+
 # window game
 window = turtle.Screen()
 window.title("Snake.exe")
 window.bgcolor("#7FB300")
-window.setup(width=640, height=480, startx=None, starty=None)
+window.setup(width=800, height=600, startx=None, starty=None)
 window.tracer(0)
     
 # snake head 
@@ -37,18 +42,35 @@ bonus.goto(0, 100)
 # body segments
 segments = []
 
+
+# Score counter
+score = turtle.Turtle()
+score.speed(0)
+score.shape("square")
+score.color("white")
+score.penup()
+score.hideturtle()
+score.goto(0, 268)
+score.write("Score: 0   Highest Score: 0", align = "center", font = ("Slab Serif", 22, "normal"))
+
 # movement functions
 def go_up():
-    head.direction = "up"
+    if head.direction != "down":
+        head.direction = "up"
 
 def go_down():
-    head.direction = "down"
+    if head.direction != "up":
+        head.direction = "down"
 
 def go_left():
-    head.direction = "left"
+    if head.direction != "right":
+        head.direction = "left"
 
 def go_right():
-    head.direction = "right" 
+    if head.direction != "left":
+        head.direction = "right" 
+def exit():
+    head.direction = "e"
 
 def move():
     # switching the head and body direction to up
@@ -56,9 +78,6 @@ def move():
         turtleAvatar = "headup.gif"
         window.addshape(turtleAvatar) 
         head.shape(turtleAvatar)
-
-        seg = "bg12.gif"
-
         y = head.ycor()
         head.sety(y + 20)
 
@@ -89,6 +108,9 @@ def move():
         x = head.xcor()
         head.setx(x + 20)
 
+    if head.direction == "e":
+        window.bye()
+
 
 # key bindings
 window.listen()
@@ -96,20 +118,41 @@ window.onkeypress(go_up, "w")
 window.onkeypress(go_down, "s")
 window.onkeypress(go_left, "a")
 window.onkeypress(go_right, "d")
+window.onkeypress(exit, "e")
 
 # main loop game
 while True:
     window.update()
 
+    # Collision with the borders
+    if head.xcor() > 370 or head.xcor() < -380 or head.ycor()  > 285.5 or head.ycor() < -265:
+        time.sleep(1)
+        head.goto(0, 0)
+        head.direction = "stop"
+
+    
+
+        # Move an clear the list when the snake hits the borders
+        for segment in segments:
+            segment.goto(2000, 2000)
+        segments.clear()
+
+
+        # score reset
+        score_count = 0
+        score.clear()
+        score.write("Score: {}    Highest Score: {}".format(score_count, highest_score), align = "center", font = ("Slab Serif", 22, "normal"))
+    
+
     # Collision with segment
     if head.distance(bonus) < 20:
         # randomize the bonus on the screen
-        x = random.randint(-310, 310)
-        y = random.randint(-205, 205)
+        x = random.randint(-375, 375)
+        y = random.randint(-285, 285)
         bonus.goto(x, y)
 
         # Adding segments
-        seg = "bg12.gif"
+        seg = "bg123.gif"
         new_segment = turtle.Turtle()
         new_segment.speed(0)
         window.addshape(seg)
@@ -117,13 +160,23 @@ while True:
         new_segment.color("#303324")
         new_segment.penup()
         segments.append(new_segment)
-    
 
-    # Body incresing
+
+        # incresing the score
+        score_count += 5
+
+        if score_count > highest_score:
+            highest_score = score_count
+        score.clear()
+        score.write("Score: {}    Highest Score: {}".format(score_count, highest_score), align = "center", font = ("Slab Serif", 22, "normal"))
+    
+    # Body incresing an score
     for index in range(len(segments) - 1, 0, -1):
         x = segments[index - 1].xcor()
         y = segments[index - 1].ycor()
         segments[index].goto(x, y)
+
+
 
     # Incresing with the first segment
     if len(segments) > 0:
@@ -133,6 +186,24 @@ while True:
 
 
     move()
+
+      # Checking for body collision
+    for segment in segments:
+        if segment.distance(head) < 20:
+            time.sleep(1)
+            head.goto(0, 0)
+            head.direction = "stop"
+
+            # Game interrupting
+            for segment in segments:
+                segment.goto(2000, 2000)
+
+            segments.clear()
+
+             # score reset
+            score_count = 0
+           
     time.sleep(delay)
 
 window.mainloop()
+    
